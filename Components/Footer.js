b70ef21b7dad64796b0111adc8e1d0c69798eb86
DONALDBZR <andygaspard@hotmail.com>
2023-11-02 19:44:49 +0400

    [UPDATE 0.0.34] The Footer component of the application.

---
+++ Components/Footer.js
@@ -0,0 +1,74 @@
+/**
+ * The Footer component of the application.
+ */
+class Footer extends React.Component {
+    /**
+     * Initializing the component which will import set the data
+     * needed to operate
+     * @returns {Footer}
+     */
+    constructor(props) {
+        super(props);
+        /**
+         * The states of the application
+         * @type {{ System: { route: string, color_scheme: string, status: number } }}
+         */
+        this.state = {
+            System: {
+                route: "",
+                color_scheme: "",
+                status: 200,
+            },
+        };
+    }
+    
+    /**
+     * Running the methods needed as soon as the component has been
+     * successfully mounted.
+     * @returns {void}
+     */
+    componentDidMount() {
+        this.getRoute();
+        this.getStatus();
+    }
+
+    /**
+     * Setting the view route of the application.
+     * @returns {void}
+     */
+    getRoute() {
+        this.setState((previous) => ({
+            System: {
+                ...previous.System,
+                route: window.location.pathname,
+            },
+        }));
+    }
+    
+    /**
+     * Retrieve the Status of the page.
+     * @returns {void}
+     */
+    getStatus() {
+        this.setState((previous) => ({
+            System: {
+                ...previous.System,
+                status: Number(document.body.attributes["status"].value)
+            }
+        }));
+    }
+
+    /**
+     * Renders the components that are being returned
+     * @returns {HTMLFooterElement}
+     */
+    render() {
+        return (
+            <>
+                <object data="/Static/SVG/Logo.svg" type="image/svg+xml"></object>
+            </>
+        );
+    }
+}
+
+ReactDOM.render(<Footer />, document.querySelector("footer"));
