3763cf813d5ee7ff499396e724d736b0dd7b101b
DONALDBZR <andygaspard@hotmail.com>
2023-11-02 14:44:27 +0400

    [UPDATE 0.0.11] Rerouting the template

--- Views/HTTP404.php
+++ Views/HTTP404.php
@@ -1,3 +1,3 @@
 <?php
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Configs/Routes.php";
-require_once "{$_SERVER['DOCUMENT_ROOT']}/Public/Templates/Template.html";
+require_once "{$_SERVER['DOCUMENT_ROOT']}/Templates/Template.html";
