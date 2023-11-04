d95a45bc228a9704c14581645cd4d313efa05db7
DONALDBZR <andygaspard@hotmail.com>
2023-11-04 17:34:28 +0400

    [UPDATE 0.1.15] It is the controller which controls the sub-directory for the Access portal.

---
+++ Controllers/Access.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * It is the controller which controls the sub-directory for
+ * the Access portal.
+ */
+class Access
+{
+    public function __construct() {
+        echo "<pre>";
+        var_dump($_SERVER);
+    }
+}
