fba648f8f15f344a8469ceb6ef2ed9fc7f7283cf
DONALDBZR <andygaspard@hotmail.com>
2023-11-04 17:31:40 +0400

    [UPDATE 0.1.14] Adding the timezone for the server's location

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+date_default_timezone_set("Indian/Mauritius");
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Configs/Routes.php";
