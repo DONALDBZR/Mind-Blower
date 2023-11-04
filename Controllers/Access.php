a671177ccceff69cfa88b0d575047d38e0a62357
DONALDBZR <andygaspard@hotmail.com>
2023-11-04 17:44:08 +0400

    [UPDATE 0.1.16] The request uniform resource information of the hypertext transfer protocol.

--- Controllers/Access.php
+++ Controllers/Access.php
@@ -5,8 +5,28 @@
  */
 class Access
 {
+    /**
+     * The request uniform resource information of the hypertext
+     * transfer protocol.
+     * @var string $request_uri
+     */
+    private string $request_uri;
+
     public function __construct() {
-        echo "<pre>";
+        $name = get_class($this);
+        $portal_address = "/{$name}";
+        $this->setRequestUri(str_replace($portal_address, "", $_SERVER["REQUEST_URI"]));
+        echo "<pre>Class Name: {$name}<br />Portal Address: {$portal_address}<br />Route: {$this->getRequestUri()}<br />";
         var_dump($_SERVER);
+    }
+
+    public function getRequestUri(): string
+    {
+        return $this->request_uri;
+    }
+
+    public function setRequestUri(string $request_uri): void
+    {
+        $this->request_uri = $request_uri;
     }
 }
